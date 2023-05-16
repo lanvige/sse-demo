b9691d5f5584b18b2f46c3d5cb35c678a34b2ba1
Zhiming Jiang <lanvige@gmail.com>
2023-05-16 23:01:29 +0800

    refactor openai

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,187 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { MutableRefObject, memo, useCallback, useContext, useEffect, useRef, useState } from 'react';
+
+import Image from 'next/image';
+import styles from './page.module.css';
+
+import { createParser } from 'eventsource-parser';
+import { debug } from 'console';
 
 export default function Home() {
+  const [currentMessage, setCurrentMessage] = useState<string>('');
+
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  function Uint8ArrayToString(array: any) {
+    var out, i, len, c;
+    var char2, char3;
+
+    out = '';
+    len = array.length;
+    i = 0;
+    while (i < len) {
+      c = array[i++];
+      switch (c >> 4) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+          // 0xxxxxxx
+          out += String.fromCharCode(c);
+          break;
+        case 12:
+        case 13:
+          // 110x xxxx   10xx xxxx
+          char2 = array[i++];
+          out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
+          break;
+        case 14:
+          // 1110 xxxx  10xx xxxx  10xx xxxx
+          char2 = array[i++];
+          char3 = array[i++];
+          out += String.fromCharCode(((c & 0x0f) << 12) | ((char2 & 0x3f) << 6) | ((char3 & 0x3f) << 0));
+          break;
+      }
+    }
+
+    return out;
+  }
+
+  const fetchStream = async (url: string, params: any) => {
+    debugger;
+    const { onmessage, onclose, ...otherParams } = params;
+
+    // ????
+    const pump = async (controller: any, reader: any) => {
+      const { value, done } = await reader.read();
+
+      // debugger;
+      // if (done) {
+      //   controller.close();
+      //   onclose?.();
+      // } else {
+      //   const s = Uint8ArrayToString(value);
+      //   debugger;
+      //   await onmessage?.(s);
+      //   // Enqueue the next data chunk into our target stream
+      //   controller.enqueue(value);
+      //   // ?????
+      //   pump(controller, reader);
+      // }
+
+      const parseCallback = async (event: any) => {
+        // 只处理 event 类型
+        if (event.type === 'event') {
+          const data = event.data;
+
+          debugger;
+
+          if (data === '[DONE]') {
+            controller.close();
+            return;
+          }
+
+          try {
+            const json = JSON.parse(data);
+            if (json.content) {
+              debugger;
+              // const text = json.choices[0].delta.content;
+              const text = json.content;
+              debugger;
+              await onmessage?.(text);
+            }
+          } catch (e) {
+            controller.error(e);
+          }
+        }
+      };
+
+      const parser = createParser(parseCallback);
+      parser.feed(decoder.decode(value));
+
+      // for await (const chunk of value) {
+      //   // parser.feed(decoder.decode(chunk));
+      //   // await onmessage?.(chunk);
+      //   debugger;
+      // }
+
+      // const queue = encoder.encode(text);
+      // controller.enqueue(queue);
+      controller.enqueue(reader);
+      // ????
+      pump(controller, reader);
+    };
+
+    // https://developer.mozilla.org/zh-CN/docs/Web/API/Streams_API/Using_readable_streams
+    // 发送请求，使用参数中的 header
+    return (
+      fetch(url, otherParams)
+        .then((response: any) => {
+          // 以 ReadableStream 解析数据
+          const reader = response.body.getReader();
+
+          const stream = new ReadableStream({
+            start(controller) {
+              pump(controller, reader);
+            },
+          });
+
+          return stream;
+        })
+        // Create a new response out of the stream
+        .then((stream) => {
+          // ?????
+          new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text();
+        })
+      // .then((response) => response.blob())
+      // .catch((err) => console.error(err))
+    );
+  };
+
+  const bodyStr = JSON.stringify({
+    messages: [
+      {
+        role: 'user',
+        content: '作为一名父亲，如何和女儿进行沟通',
+      },
+    ],
+  });
+
+  fetchStream('http://localhost:8701/uv1/chat2', {
+    method: 'POST',
+    headers: {
+      accept: 'text/event-stream',
+      'Content-Type': 'application/json',
+    },
+    body: bodyStr,
+    onmessage: (res: string) => {
+      // todo
+      // const queue = encoder.encode(res);
+      const newmessge = currentMessage + res;
+
+      debugger;
+      setCurrentMessage(newmessge);
+      console.log(res);
+    },
+    onclose: (res: string) => {
+      // todo
+      debugger;
+      setCurrentMessage(currentMessage + '已关闭');
+      console.log(res);
+    },
+  });
+
+  // ================== res
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div>{currentMessage}</div>
     </main>
-  )
+  );
 }
