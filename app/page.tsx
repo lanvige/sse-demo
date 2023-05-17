440d96fc6d9ec316a53e06330df5581d841f3fdd
Zhiming Jiang <lanvige@gmail.com>
2023-05-17 11:16:30 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -54,68 +54,94 @@ export default function Home() {
   }
 
   const fetchStream = async (url: string, params: any) => {
-    debugger;
     const { onmessage, onclose, ...otherParams } = params;
 
     // ????
     const pump = async (controller: any, reader: any) => {
+      // 调用 read() ，它返回一个包含 results 对象的 Promise ——它包含我们读取的结果，形式为 { done, value }
       const { value, done } = await reader.read();
 
-      // debugger;
+      debugger;
+
       // if (done) {
       //   controller.close();
       //   onclose?.();
       // } else {
       //   const s = Uint8ArrayToString(value);
+      //   const s2 = decoder.decode(value);
       //   debugger;
       //   await onmessage?.(s);
       //   // Enqueue the next data chunk into our target stream
       //   controller.enqueue(value);
-      //   // ?????
       //   pump(controller, reader);
       // }
 
+      // ??? Uncaught (in promise) TypeError: Failed to execute 'close' on 'ReadableStreamDefaultController': Cannot close a readable stream that has already been requested to be closed
+      if (done) {
+        debugger;
+        // controller.close();
+        onclose?.();
+        return;
+      }
+
+      // 替换旧的方法，用一个库来处理这个
       const parseCallback = async (event: any) => {
+        debugger;
         // 只处理 event 类型
         if (event.type === 'event') {
           const data = event.data;
+          const eventId = event.id;
 
-          debugger;
+          if (eventId === '[TOKENS]') {
+            return;
+          }
+
+          // debugger;
 
           if (data === '[DONE]') {
-            controller.close();
+            // controller.close();
             return;
           }
 
           try {
             const json = JSON.parse(data);
             if (json.content) {
-              debugger;
+              // debugger;
               // const text = json.choices[0].delta.content;
               const text = json.content;
-              debugger;
-              await onmessage?.(text);
+              // debugger;
+              onmessage?.(text);
+
+              // 将下一个数据块排队到我们的目标流中
+              // const queue = encoder.encode(text);
+              // controller.enqueue(queue);
+              if (eventId === '[DONE]') {
+                const queue = encoder.encode(text);
+                controller.enqueue(queue);
+                await pump(controller, reader);
+                return;
+              }
             }
-          } catch (e) {
-            controller.error(e);
+          } catch (err) {
+            debugger;
+            controller.error(err);
           }
         }
       };
 
       const parser = createParser(parseCallback);
-      parser.feed(decoder.decode(value));
+      await parser.feed(decoder.decode(value));
 
-      // for await (const chunk of value) {
-      //   // parser.feed(decoder.decode(chunk));
-      //   // await onmessage?.(chunk);
-      //   debugger;
-      // }
-
-      // const queue = encoder.encode(text);
-      // controller.enqueue(queue);
-      controller.enqueue(reader);
-      // ????
-      pump(controller, reader);
+      for await (const chunk of value) {
+        // parser.feed(decoder.decode(chunk));
+        // onmessage?.(chunk);
+        // debugger;
+      }
+      debugger;
+      const b = decoder.decode(value);
+      console.log(b);
+      // controller.enqueue(value);
+      // await pump(controller, reader);
     };
 
     // https://developer.mozilla.org/zh-CN/docs/Web/API/Streams_API/Using_readable_streams
@@ -134,13 +160,14 @@ export default function Home() {
 
           return stream;
         })
+        // 从流中创建一个新的响应
         // Create a new response out of the stream
         .then((stream) => {
           // ?????
           new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text();
         })
-      // .then((response) => response.blob())
-      // .catch((err) => console.error(err))
+        // .then((response) => response.blob())
+        .catch((err) => console.error(err))
     );
   };
 
@@ -148,7 +175,7 @@ export default function Home() {
     messages: [
       {
         role: 'user',
-        content: '作为一名父亲，如何和女儿进行沟通',
+        content: '作为一名父亲，如何和女儿进行沟通，给出4个字的回答',
       },
     ],
   });
@@ -163,16 +190,14 @@ export default function Home() {
     onmessage: (res: string) => {
       // todo
       // const queue = encoder.encode(res);
-      const newmessge = currentMessage + res;
-
-      debugger;
-      setCurrentMessage(newmessge);
+      // debugger;
+      setCurrentMessage((r: any) => r + res);
       console.log(res);
     },
     onclose: (res: string) => {
       // todo
       debugger;
-      setCurrentMessage(currentMessage + '已关闭');
+      setCurrentMessage((r: any) => r + '已关闭');
       console.log(res);
     },
   });
